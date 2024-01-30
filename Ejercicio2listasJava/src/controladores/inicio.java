210e0464edaaaca8242c7e0ba2c30a476e9e98b2
Jaqueline <csi23-jromres@colegioaltair.net>
2024-01-30 12:07:26 +0100

    no message

---
+++ Ejercicio2listasJava/src/controladores/inicio.java
@@ -0,0 +1,31 @@
+package controladores;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class inicio {
+
+	public inicio() {
+		
+		Scanner sc = new Scanner(System.in);
+		List<Integer> listaNumeros;
+		listaNumeros= new ArrayList<Integer>();
+	
+		public void pedirNumero(List<Integer> listaNumeros){
+			System.out.println("Introduzca número entero");
+			int numeroIntroducido = sc.nextInt();
+			listaNumeros.add(numeroIntroducido);
+		}
+		bool introducir=false;
+		if(!introducir==)
+		
+		public void mostrarLista(List<Integer>ListaNumeros){
+			for ( Object num : listaNumeros) {
+				System.out.println(num);
+			}
+
+		}
+		
+	}
+}
